2d65ab85d61635260ab997e49bf6ef04bc4e69d9
axel.grude@gmail.com <axel.grude@gmail.com@35eaf550-ba74-951e-5021-6338d086feaf>
2012-06-10 07:33:29 +0000

    move init() code into its own module

---
+++ branches/0.8/content/smartTemplate-init.js
@@ -0,0 +1,5 @@
+
+window.setTimeout ( function(){
+	gSmartTemplate.init();
+  document.getElementById("msgcomposeWindow").addEventListener("compose-window-init", gSmartTemplate.initListner, false);
+},10 );
