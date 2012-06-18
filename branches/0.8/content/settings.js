69e4edbb9fdf622079bb4de578599f513e4c621b
axel.grude@gmail.com <axel.grude@gmail.com@35eaf550-ba74-951e-5021-6338d086feaf>
2012-06-18 11:16:03 +0000

    fixed Deck problem (all field changes from help window always only affected in common panel even if another account was selected)

--- branches/0.8/content/settings.js
+++ branches/0.8/content/settings.js
@@ -1,4 +1,4 @@
-"use strict"; 
+"use strict";
 
 // -----------------------------------------------------------------------------------
 // ---------------------------- last edit at 06/10/2012 ------------------------------
@@ -50,7 +50,7 @@ SmartTemplate4.Settings = {
 		}
 		return arguments[0];
 	} ,
-	
+
 	// Select Deck with identity key
 	//--------------------------------------------------------------------
 	prefDeck : function(id, index)
@@ -58,35 +58,35 @@ SmartTemplate4.Settings = {
 		var deck = document.getElementById(id + this.gCurId);
 		if (deck)
 		  { deck.selectedIndex = index; }
-		  
+
 		const idkey = document.getElementById("msgIdentity").value;
 		const branch = idkey == "common" ? "" : "." + idkey;
 	} ,
-	
-	
+
+
 	// Return checkbox is checked or not
 	//--------------------------------------------------------------------
 	isChecked : function(elid)
 	{
 		return document.getElementById(elid).checked;
 	} ,
-	
-	
+
+
 	// Disable DOM node depeding on checkboxes
 	//--------------------------------------------------------------------
 	disableWithCheckbox : function()
-	{  
+	{
 		if (this.prefDisable(this.isChecked("new" + this.gCurId), "newmsg", "newhtml", "newnbr")) {
-			this.prefDisable(this.isChecked("newhtml" + this.gCurId), "newnbr"); 
+			this.prefDisable(this.isChecked("newhtml" + this.gCurId), "newnbr");
 		}
-		if (this.prefDisable(this.isChecked("rsp" + this.gCurId), "rspmsg", "rsphtml", "rspnbr", "rsphead")) { 
-			this.prefDisable(this.isChecked("rsphtml" + this.gCurId), "rspnbr"); 
+		if (this.prefDisable(this.isChecked("rsp" + this.gCurId), "rspmsg", "rsphtml", "rspnbr", "rsphead")) {
+			this.prefDisable(this.isChecked("rsphtml" + this.gCurId), "rspnbr");
 		}
-		if (this.prefDisable(this.isChecked("fwd" + this.gCurId), "fwdmsg", "fwdhtml", "fwdnbr", "fwdhead")) { 
-			this.prefDisable(this.isChecked("fwdhtml" + this.gCurId), "fwdnbr"); 
+		if (this.prefDisable(this.isChecked("fwd" + this.gCurId), "fwdmsg", "fwdhtml", "fwdnbr", "fwdhead")) {
+			this.prefDisable(this.isChecked("fwdhtml" + this.gCurId), "fwdnbr");
 		}
 	},
-	
+
 	// Delete unused preferences.
 	//--------------------------------------------------------------------
 	cleanupUnusedPrefs : function()
@@ -98,16 +98,16 @@ SmartTemplate4.Settings = {
 			}
 		}
 	} ,
-	
+
 	//******************************************************************************
 	// Preferences library
 	//******************************************************************************
-	
+
 	// Create preferences
 	//--------------------------------------------------------------------
 	setPref1st : function(prefbranch)
 	{
-		try { 
+		try {
 			this.prefService.getBoolPref(prefbranch + "def")
 		} catch(e) { this.prefService.setBoolPref(prefbranch + "def", true); }
 		try {
@@ -153,7 +153,7 @@ SmartTemplate4.Settings = {
 			this.prefService.getBoolPref(prefbranch + "fwdhead");
 		} catch(e) { this.prefService.setBoolPref(prefbranch + "fwdhead", false); }
 	} ,
-	
+
 	// Get preference without prefType
 	getPref : function(prefstring)
 	{
@@ -171,7 +171,7 @@ SmartTemplate4.Settings = {
 		}
 		return false;
 	} ,
-	
+
 	// Set preference without prefType
 	//--------------------------------------------------------------------
 	setPref : function(prefstring, value)
@@ -189,8 +189,8 @@ SmartTemplate4.Settings = {
 		}
 		return false;
 	} ,
-	
-	
+
+
 	// Reload preferences and update elements.
 	//--------------------------------------------------------------------
 	reloadPrefs : function(el)
@@ -204,11 +204,11 @@ SmartTemplate4.Settings = {
 			el = el.nextSibling;
 		}
 	} ,
-	
+
 	//******************************************************************************
 	// Preferences
 	//******************************************************************************
-	
+
 	// Setup default preferences and common settings
 	//--------------------------------------------------------------------
 	onLoad : function() // mod 0.3.2
@@ -216,55 +216,62 @@ SmartTemplate4.Settings = {
 		// Check and set common preference
 		this.setPref1st("extensions.smarttemplate.");
 		this.disableWithCheckbox();
-	
+
 		// Set account popup
 		this.fillIdentityListPopup(); 				// mod 0.3.2
-	
+
 		this.cleanupUnusedPrefs();
-	
+
 		// Switch account (from account setting)  // add 0.4.0
-		if (window.arguments && window.arguments.length >= 1)	{  
-		  this.switchIdentity(window.arguments[0]); 
+		if (window.arguments && window.arguments.length >= 1)	{
+		  this.switchIdentity(window.arguments[0]);
 		}
-	
+
 		// disable Use default (common account)
 		document.getElementById("use_default").setAttribute("disabled", "true");
-		
-		window.onCodeWord = function(code) { 
-			SmartTemplate4.Util.logDebugOptional("events","Preferences window retrieved code variable: " + code);
-			
-			let tabbox = document.getElementById('deckB.nodef');
-			let templateMsgBoxId = '';
-			switch (tabbox.selectedIndex) {
-				case 0:
-					templateMsgBoxId='newmsg';
-					break;
-				case 1:
-					templateMsgBoxId='rspmsg';
-					break;
-				case 2:
-					templateMsgBoxId='fwdmsg';
-					break;
-				default: // unknown!
-					break;
-			}
-			if (templateMsgBoxId) {
-				let editor = document.getElementById(templateMsgBoxId);
-				SmartTemplate4.Settings.insertAtCaret(editor, code);
-			}
+
+		window.onCodeWord = function(code) {
+			SmartTemplate4.Settings.onCodeWord(code);
 		};
-		
+
 		return true;
 	} ,
-	
+
+	onCodeWord : function(code) {
+		SmartTemplate4.Util.logDebugOptional("events","Preferences window retrieved code variable: " + code);
+
+		let currentDeck = (SmartTemplate4.Settings.gCurId) ? 'deckB.nodef' + SmartTemplate4.Settings.gCurId : 'deckB.nodef';
+		let tabbox = document.getElementById(currentDeck);
+		let templateMsgBoxId = '';
+		switch (tabbox.selectedIndex) {
+			case 0:
+				templateMsgBoxId='newmsg';
+				break;
+			case 1:
+				templateMsgBoxId='rspmsg';
+				break;
+			case 2:
+				templateMsgBoxId='fwdmsg';
+				break;
+			default: // unknown!
+				break;
+		}
+		if (templateMsgBoxId) {
+			if (SmartTemplate4.Settings.gCurId)
+				templateMsgBoxId += SmartTemplate4.Settings.gCurId;
+			let editBox = document.getElementById(templateMsgBoxId);
+			SmartTemplate4.Settings.insertAtCaret(editBox, code);
+		}
+	} ,
+
 	onUnload : function() {
 // 		document.removeEventListener("SmartTemplate4CodeWord", SmartTemplate4.Listener.listen, false);
 	} ,
-	
+
 	// Setup cloned nodes and replace preferences strings
 	//--------------------------------------------------------------------
 	prefCloneAndSetup : function(el, str, key)
-	{  
+	{
 		function replaceAttr(_el, _attrname, _str, _key) { try {
 			if (_el.hasAttribute(_attrname)) {
 				var _attr = _el.getAttribute(_attrname);
@@ -276,14 +283,14 @@ SmartTemplate4.Settings = {
 			if (_el.hasAttribute(_attrname))
 			  { _el.setAttribute(_attrname, _el.getAttribute(_attrname) + _key); }
 		} catch(ex) {} }
-	
+
 		var deps = 0;
 		while (el) {
 			// Set id, name, prefname
 			appendAttr(el, "id", key);
 			replaceAttr(el, "name", str, key);
 			appendAttr(el, "preference", key);
-	
+
 			// Get next node or parent's next node
 			if (el.hasChildNodes()) {
 				el = el.firstChild;
@@ -297,65 +304,65 @@ SmartTemplate4.Settings = {
 			}
 		}
 	} ,
-	
+
 	//******************************************************************************
 	// Identity
 	//******************************************************************************
-	
+
 	// Add identity
 	//--------------------------------------------------------------------
 	addIdentity : function(menuvalue)
-	{  
+	{
 		const  branch = menuvalue == "common" ? "" : "." + menuvalue;
-	
+
 		// Add preferences, if preferences is not create.
 		this.setPref1st("extensions.smarttemplate" + branch + ".");
-	
+
 		// Clone and setup a preference window tags.
 		const el = document.getElementById("deckA.per_account");
 		const clone = el.cloneNode(true);
-	
+
 		this.prefCloneAndSetup(clone, "smarttemplate", branch);
 		el.parentNode.appendChild(clone);				// mod 0.3.2
-	
+
 		// Reload preferences
 		this.reloadPrefs(document.getElementById("smarttemplate" + branch));
-	
+
 		// Disabled or Hidden DOM node
 		this.gCurId = branch;    // change current id for pref library
 		this.prefDeck("default.deckB", this.isChecked("use_default" + branch)?1:0);
-		
+
 		this.disableWithCheckbox();
 		this.gCurId = "";
 	} ,
-	
-	
+
+
 	// Fill identities menu
 	//--------------------------------------------------------------------
 	fillIdentityListPopup : function()	// mod 0.3.2
-	{  
+	{
 		SmartTemplate4.Util.logDebugOptional("settings","fillIdentityListPopup()");
 		const accounts = Components.classes["@mozilla.org/messenger/account-manager;1"].
 									  getService(this.Ci.nsIMsgAccountManager).accounts;
-	
+
 		for (var idx = 0; idx < accounts.Count(); idx++) {
 			const account = accounts.QueryElementAt(idx, this.Ci.nsIMsgAccount);
-	
+
 			if (!account.incomingServer)
-				continue; 
-	
+				continue;
+
 			for (var j = 0; j < account.identities.Count(); j++) {
 				const identity = account.identities.QueryElementAt(j, this.Ci.nsIMsgIdentity);
 				document.getElementById("msgIdentity")
-						.appendItem(account.incomingServer.prettyName 
-									+ " - " 
+						.appendItem(account.incomingServer.prettyName
+									+ " - "
 									+ identity.identityName, identity.key, "");
 				this.addIdentity(identity.key);
 			}
 		}
 	} ,
-	
-	
+
+
 	// Switch Identity (from account setting window)		// add 0.4.0 S
 	//--------------------------------------------------------------------
 	switchIdentity : function(idKey)
@@ -374,13 +381,13 @@ SmartTemplate4.Settings = {
 			el = el.nextSibling; index++;
 		}
 	} , // add 0.4.0 E
-	
+
 	// Select identity (from xul)
 	//--------------------------------------------------------------------
 	selectIdentity : function(idkey)
-	{  
+	{
 		const  branch = idkey == "common" ? "" : "." + idkey;
-			
+
 		// Display identity.
 		var deck = document.getElementById("account.deckA");	// mod 0.3.2 S
 		var index = 0;
@@ -391,10 +398,10 @@ SmartTemplate4.Settings = {
 				break;
 			}
 			index++;
-		} 
-		
+		}
+
 	} ,
-	
+
 	insertAtCaret : function insertAtCaret(element, code) {
 		// code = code + ' '; // this could be an option in a future version...
 		if (!element) {
@@ -435,9 +442,9 @@ SmartTemplate4.Settings = {
 		catch(e) {
 			SmartTemplate4.Util.logException("Exception in insertAtCaret; (nodeName=" + node  +")", e);
 		}
-	}	
-	
-	
+	}
+
+
 
 
 };
