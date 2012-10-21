a953490dcabb43399b3b4a7bcec0402d26bf0c80
marc.pueschel <marc.pueschel@35eaf550-ba74-951e-5021-6338d086feaf>
2012-10-21 14:10:46 +0000

    add a new default settings for our new install.rdf file

--- defaults/preferences/smartTemplate-defaults.js
+++ defaults/preferences/smartTemplate-defaults.js
@@ -1,23 +1,47 @@
+/* model */
+pref("extensions.smarttemplate4@thunderbird.extension.description", "chrome://smartTemplate4/locale/settings.properties");
+
 /* global settings */
+
 pref("extensions.smartTemplate4.firstRun", true);
+
 pref("extensions.smartTemplate4.version", "?");
+
 pref("extensions.smartTemplate4.insertBRatTop", false);
+
+
 /* this is a convenience function - if I change from after or during composing, delete sig */
+
 pref("extensions.smartTemplate4.removeSigOnIdChangeAfterEdits", false);
 
 
+
+
 /* debug settings */
+
 pref("extensions.smartTemplate4.debug", false);
+
 pref("extensions.smartTemplate4.debug.events", false);
+
 pref("extensions.smartTemplate4.debug.functions", false);
+
 pref("extensions.smartTemplate4.debug.functions.delReplyHeader", false);
+
 pref("extensions.smartTemplate4.debug.functions.delForwardHeader", false);
+
 pref("extensions.smartTemplate4.debug.functions.insertTemplate", false);
+
 pref("extensions.smartTemplate4.debug.deleteNodes", false);
+
 pref("extensions.smartTemplate4.debug.settings", false);
+
 pref("extensions.smartTemplate4.debug.regularize", false);
+
 pref("extensions.smartTemplate4.debug.mime", false);
+
 pref("extensions.smartTemplate4.debug.firstRun", false);
+
 pref("extensions.smartTemplate4.debug.test.update", false);
+
 pref("extensions.smartTemplate4.debug.identities", false);
 
