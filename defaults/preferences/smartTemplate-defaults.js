545ab443c3b3d0b0fd91e32dca6a0c5e8c94c32d
axel.grude <axel.grude@gmail.com>
2012-10-27 16:17:57 +0000

    added font size setting

--- defaults/preferences/smartTemplate-defaults.js
+++ defaults/preferences/smartTemplate-defaults.js
@@ -7,7 +7,7 @@ pref("extensions.smartTemplate4.version", "?");
 pref("extensions.smartTemplate4.breaksAtTop", 1);
 pref("extensions.smartTemplate4.showStatusIcon", true);
 pref("extensions.smartTemplate4.statusIconLabelMode", 1); /* 0 - no label; 1 - autohide; 2 - always show */
-
+pref("extensions.smartTemplate4.font.size", 8); /* 8pt .templateBox font size */
 /* this is a convenience function - if I change from after or during composing, delete sig */
 pref("extensions.smartTemplate4.removeSigOnIdChangeAfterEdits", false);
 
