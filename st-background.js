fdf223de5ce4f2b8f5d498fa66b865313ed59d32
Klaus Buecher <kbuecher@optosolar.com>
2020-08-27 22:41:13 +0200

    onboarding/upboarding page

--- st-background.js
+++ st-background.js
@@ -9,7 +9,36 @@
 
 
  async function main() {
-    messenger.WindowListener.registerDefaultPrefs("chrome/content/scripts/smartTemplate-defaults.js");
+    
+   
+   
+   messenger.runtime.onInstalled.addListener(async ({ reason, temporary }) => {
+     // if (temporary) return; // skip during development
+      switch (reason) {
+        case "install":
+          {
+            const url = browser.runtime.getURL("popup/installed.html");
+            //await browser.tabs.create({ url });
+            await browser.windows.create({ url, type: "popup", height: 600, width: 600, });
+          }
+          break;
+        // see below
+        case "update":
+          {
+            const url = browser.runtime.getURL("popup/update.html");
+            //await browser.tabs.create({ url });
+            await browser.windows.create({ url, type: "popup", height: 600, width: 600, });
+          }
+          break;
+        // see below
+        }
+    });
+   
+   
+   
+   
+   
+   messenger.WindowListener.registerDefaultPrefs("chrome/content/scripts/smartTemplate-defaults.js");
     
     messenger.WindowListener.registerChromeUrl([ 
         ["content", "smarttemplate4", "chrome/content/"],
