06ac62e1288f7959a6261c76e9ba6c01c03d6f69
RealRaven2000 <axel.grude@gmail.com>
2020-10-16 20:03:30 +0100

    popup: made the background image element less high because it somehow covers the links!

--- st-background.js
+++ st-background.js
@@ -9,74 +9,74 @@
 
 
  async function main() {
-    
    
-   
-   messenger.runtime.onInstalled.addListener(async ({ reason, temporary }) => {
-     // if (temporary) return; // skip during development
-      switch (reason) {
-        case "install":
-          {
-            const url = browser.runtime.getURL("popup/installed.html");
-            //await browser.tabs.create({ url });
-            await browser.windows.create({ url, type: "popup", width: 910, height: 750, });
-          }
-          break;
-        // see below
-        case "update":
-          {
-            const url = browser.runtime.getURL("popup/update.html");
-            //await browser.tabs.create({ url });
-            await browser.windows.create({ url, type: "popup", width: 910, height: 810, });
-          }
-          break;
-        // see below
+  messenger.runtime.onInstalled.addListener(async ({ reason, temporary }) => {
+    // if (temporary) return; // skip during development
+    switch (reason) {
+      case "install":
+        {
+          const url = browser.runtime.getURL("popup/installed.html");
+          //await browser.tabs.create({ url });
+          await browser.windows.create({ url, type: "popup", width: 910, height: 750, });
         }
+        break;
+      // see below
+      case "update":
+        {
+          const url = browser.runtime.getURL("popup/update.html");
+          //await browser.tabs.create({ url });
+          let screenH = window.screen.height,
+              windowHeight = (screenH > 920) ? 920 : screenH;
+          await browser.windows.create({ url, type: "popup", width: 910, height: windowHeight, });
+        }
+        break;
+      // see below
+      }
     });
    
    
    
    
    
-    messenger.WindowListener.registerDefaultPrefs("chrome/content/scripts/smartTemplate-defaults.js");
-    
-    messenger.WindowListener.registerChromeUrl([ 
-        ["content", "smarttemplate4", "chrome/content/"],
-        ["resource", "smarttemplate4", "chrome/content/"],
-        ["locale", "smarttemplate4", "en-US", "chrome/locale/en-US/"],
-        ["locale", "smarttemplate4", "cs", "chrome/locale/cs/"],
-        ["locale", "smarttemplate4", "de", "chrome/locale/de/"],
-        ["locale", "smarttemplate4", "es-ES", "chrome/locale/es-ES/"],
-        ["locale", "smarttemplate4", "fi", "chrome/locale/fi/"],
-        ["locale", "smarttemplate4", "fr", "chrome/locale/fr/"],
-        ["locale", "smarttemplate4", "id-ID", "chrome/locale/id-ID/"],
-        ["locale", "smarttemplate4", "it", "chrome/locale/it/"],
-        ["locale", "smarttemplate4", "ja", "chrome/locale/ja/"],
-        ["locale", "smarttemplate4", "nl", "chrome/locale/nl/"],
-        ["locale", "smarttemplate4", "pl", "chrome/locale/pl/"],
-        ["locale", "smarttemplate4", "pt-BR", "chrome/locale/pt-BR/"],
-        ["locale", "smarttemplate4", "ru", "chrome/locale/ru/"],
-        ["locale", "smarttemplate4", "sl", "chrome/locale/sl/"],
-        ["locale", "smarttemplate4", "sr", "chrome/locale/sr/"],
-        ["locale", "smarttemplate4", "sv-SE", "chrome/locale/sv-SE/"],
-        ["locale", "smarttemplate4", "uk", "chrome/locale/uk/"],
-        ["locale", "smarttemplate4", "zh-CN", "chrome/locale/zh-CN/"],
-        ["locale", "smarttemplate4", "zh-TW", "chrome/locale/zh-TW/"],
-        ["locale", "smarttemplate4", "oc", "chrome/locale/oc/"],
-   ]);
- 
-    messenger.WindowListener.registerOptionsPage("chrome://smarttemplate4/content/settings.xhtml"); 
-    
- //attention: each target window (like messenger.xhtml) can appear only once
- // this is different from chrome.manifest
- // xhtml for Tb78
-    
-    messenger.WindowListener.registerWindow("chrome://messenger/content/messenger.xhtml", "chrome/content/scripts/st-messenger.js");
-    messenger.WindowListener.registerWindow("chrome://messenger/content/messengercompose/messengercompose.xhtml", "chrome/content/scripts/st-composer.js");
-    messenger.WindowListener.registerWindow("chrome://messenger/content/messageWindow.xhtml", "chrome/content/scripts/st-messageWindow.js");  
+  messenger.WindowListener.registerDefaultPrefs("chrome/content/scripts/smartTemplate-defaults.js");
   
-   // messenger.WindowListener.registerStartupScript("chrome/content/scripts/qf-startup.js");
-   // messenger.WindowListener.registerShutdownScript("chrome/content/scripts/qf-shutdown.js");
+  messenger.WindowListener.registerChromeUrl([ 
+      ["content", "smarttemplate4", "chrome/content/"],
+      ["resource", "smarttemplate4", "chrome/content/"],
+      ["locale", "smarttemplate4", "en-US", "chrome/locale/en-US/"],
+      ["locale", "smarttemplate4", "cs", "chrome/locale/cs/"],
+      ["locale", "smarttemplate4", "de", "chrome/locale/de/"],
+      ["locale", "smarttemplate4", "es-ES", "chrome/locale/es-ES/"],
+      ["locale", "smarttemplate4", "fi", "chrome/locale/fi/"],
+      ["locale", "smarttemplate4", "fr", "chrome/locale/fr/"],
+      ["locale", "smarttemplate4", "id-ID", "chrome/locale/id-ID/"],
+      ["locale", "smarttemplate4", "it", "chrome/locale/it/"],
+      ["locale", "smarttemplate4", "ja", "chrome/locale/ja/"],
+      ["locale", "smarttemplate4", "nl", "chrome/locale/nl/"],
+      ["locale", "smarttemplate4", "pl", "chrome/locale/pl/"],
+      ["locale", "smarttemplate4", "pt-BR", "chrome/locale/pt-BR/"],
+      ["locale", "smarttemplate4", "ru", "chrome/locale/ru/"],
+      ["locale", "smarttemplate4", "sl", "chrome/locale/sl/"],
+      ["locale", "smarttemplate4", "sr", "chrome/locale/sr/"],
+      ["locale", "smarttemplate4", "sv-SE", "chrome/locale/sv-SE/"],
+      ["locale", "smarttemplate4", "uk", "chrome/locale/uk/"],
+      ["locale", "smarttemplate4", "zh-CN", "chrome/locale/zh-CN/"],
+      ["locale", "smarttemplate4", "zh-TW", "chrome/locale/zh-TW/"],
+      ["locale", "smarttemplate4", "oc", "chrome/locale/oc/"],
+  ]);
+
+  messenger.WindowListener.registerOptionsPage("chrome://smarttemplate4/content/settings.xhtml"); 
+  
+  //attention: each target window (like messenger.xhtml) can appear only once
+  // this is different from chrome.manifest
+  // xhtml for Tb78
+  
+  messenger.WindowListener.registerWindow("chrome://messenger/content/messenger.xhtml", "chrome/content/scripts/st-messenger.js");
+  messenger.WindowListener.registerWindow("chrome://messenger/content/messengercompose/messengercompose.xhtml", "chrome/content/scripts/st-composer.js");
+  messenger.WindowListener.registerWindow("chrome://messenger/content/messageWindow.xhtml", "chrome/content/scripts/st-messageWindow.js");  
+
+  // messenger.WindowListener.registerStartupScript("chrome/content/scripts/qf-startup.js");
+  // messenger.WindowListener.registerShutdownScript("chrome/content/scripts/qf-shutdown.js");
 
  /*
   * Start listening for opened windows. Whenever a window is opened, the registered
@@ -86,7 +86,7 @@
   */
 
 
-    messenger.WindowListener.startListening();
+  messenger.WindowListener.startListening();
 }
 
 main();
