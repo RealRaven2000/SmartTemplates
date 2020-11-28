dc302196651035d50f69c16cca1afcf6c1e0618a
Conductor <artem.semenov@jarillolabs.com>
2020-11-28 06:57:06 +0300

    Bash version of the build script has been added.

---
+++ build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+smartTemplateRev=$( cat revision.txt | xargs )
+oldRev=$smartTemplateRev
+smartTemplateRev=$((smartTemplateRev+1))
+
+sed -i "s/pre$oldRev/pre$smartTemplateRev/g" manifest.json
+
+7z a -xr!.svn smartTemplateWeb.zip manifest.json _locales chrome locale popup st-background.js license.txt icon.png release-notes.html
+
+echo $smartTemplateRev > revision.txt
+mv smartTemplate-*.xpi "../../../Test Versions/3.1/"
+mv smartTemplateWeb.zip smartTemplate-fx-3.1pre$smartTemplateRev.xpi
