c9153ca6bac834324b80da74fa96f5c306e58ce2
RealRaven2000 <axel.grude@gmail.com>
2021-02-06 21:48:03 +0000

    2.14.2 Localization hotfix (It, Sv, Uk)

--- build.bat
+++ build.bat
@@ -6,6 +6,6 @@ pwsh -Command "(gc -en UTF8NoBOM manifest.json) -replace 'pre%oldRev%', 'pre%sma
 rem "C:\Program Files\7-Zip\7z" a -xr!.svn smartTemplate.zip install.rdf chrome.manifest content defaults locale skin license.txt icon.png 
 "C:\Program Files\7-Zip\7z" a -xr!.svn smartTemplateWeb.zip manifest.json install.rdf chrome.manifest content defaults locale skin license.txt icon.png 
 echo %smartTemplateRev% > revision.txt
-move smartTemplate-*.xpi "..\..\Test Versions\2.14\"
+move smartTemplate-*.xpi "..\..\Test Versions\2.5\"
 pwsh -Command "Start-Sleep -m 150"
-rename smartTemplateWeb.zip smartTemplate-fx-2.14.1pre%smartTemplateRev%.xpi
+rename smartTemplateWeb.zip smartTemplate-fx-2.15pre%smartTemplateRev%.xpi
