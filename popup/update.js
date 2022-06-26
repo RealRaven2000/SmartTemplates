8216fbab72d6902f7d11055f52b978d3c5ceb6a3
RealRaven2000 <axel.grude@gmail.com>
2022-06-26 20:46:37 +0100

    added Sale date + toclipboard info to splash screen

--- popup/update.js
+++ popup/update.js
@@ -194,7 +194,7 @@ const discountUpgrade = "50%";
     
     let whatsNewLst = document.getElementById('whatsNewList');
     function replaceVariableCodeTags(txt) {
-      let tags = ["header.set(to,clipboard)", "header.prefix(subject,clipboard)", "replaceText(regular Expression,clipboard)", "replaceQuotedText(regular Expression,clipboard)", "spellcheck()"];
+      let tags = ["header.set(to,clipboard)", "header.prefix(subject,clipboard)", "replaceText(regular Expression,clipboard)", "replaceQuotedText(regular Expression,clipboard)", "spellcheck()", "from(name,toclipboard)", "dateformat(\"A, e/n/Y H:M\",current,toclipboard)"];
       for (let x of tags) {
         txt = txt.replace(`%${x}%`, `<code>%${x}%</code>`);
       }
