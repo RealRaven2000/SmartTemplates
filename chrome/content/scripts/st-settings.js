d7904e6f0fcacc65d4951a34e1d43badf821e881
RealRaven2000 <axel.grude@gmail.com>
2023-07-08 18:18:01 +0100

    simplified

--- chrome/content/scripts/st-settings.js
+++ chrome/content/scripts/st-settings.js
@@ -4,17 +4,15 @@
 
 async function onLoad(activatedWhileWindowOpen) {
   const util = window.SmartTemplate4.Util;
-  if (util.versionGreaterOrEqual(util.AppverFull, "91")) {
-    util.logDebug("Inject Browser element");
-    WL.injectElements(`
-    <tabpanel flex = "1" id = "templatesFrame">
-        <browser id="templatesBrowser"
-                type="content"
-                flex = "1"
-                minheight="300"
-                remote="true"
-                />
-    </tabpanel>
-    `);
-  }
+  util.logDebug("Inject Browser element");
+  WL.injectElements(`
+  <tabpanel flex = "1" id = "templatesFrame">
+      <browser id="templatesBrowser"
+              type="content"
+              flex = "1"
+              minheight="300"
+              remote="true"
+              />
+  </tabpanel>
+  `);
 }
