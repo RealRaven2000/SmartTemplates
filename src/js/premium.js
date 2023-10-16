73f098a1accaa3b226cfc11462f0ddaa2a658658
Axel G <axel.grude@gmail.com>
2023-10-16 20:11:06 +0100

    Sales Halloween 2023

--- src/js/premium.js
+++ src/js/premium.js
@@ -1,7 +1,7 @@
 
-var sales_name = "AUTUMN SALE"; // .saleName
+var sales_name = "HALLOWEEN SALE"; // .saleName
 var sales_start_lbl = "October 17th"; // .saleStart
-var sales_end_lbl = "October 9th";  // .saleEnd
+var sales_end_lbl = "October 31st";  // .saleEnd
 var sales_end = new Date("2023-10-31");
 
 /* functions that remove elements depending on the user type (from user=pro querystring ) */
