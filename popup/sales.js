063adac611f05e951b67e435072275850b0ed2c4
RealRaven2000 <axel.grude@gmail.com>
2024-07-15 11:53:58 +0100

    4.6 official release

--- popup/sales.js
+++ popup/sales.js
@@ -4,10 +4,10 @@ const discountUpgrade = "33%";
 const discountRenewal = "25%";
 
 // used in popup.js 
-const sales_end = new Date("2024-05-29"); // Next Sale End Date (replaces endSale in popup.js)
+const sales_end = new Date("2024-07-31"); // Next Sale End Date (replaces endSale in popup.js)
 // => to localize update special-offer-expiry in messages.json!
 
-const compatibleVer = "115.9"; // Thunderbird for newsSection
+const compatibleVer = "128.*"; // Thunderbird for newsSection
 
 
 
