0382c18f5c5597f1111b1817cc063f0625ca332c
RealRaven2000 <axel.grude@gmail.com>
2024-09-03 13:13:43 +0100

    Sales date update

--- popup/sales.js
+++ popup/sales.js
@@ -1,5 +1,5 @@
 // used in popup.js 
-const sales_end = new Date("2024-09-21"); // Next Sale End Date (replaces endSale in popup.js)
+const sales_end = new Date("2024-09-22"); // Next Sale End Date (replaces endSale in popup.js)
 
 
 // used in update.js
