6bf24a03bf6c2e8d233821a485045a664705c45d
RealRaven2000 <axel.grude@gmail.com>
2025-02-23 21:12:18 +0000

    issue #338
    Set Fallback charset setting to UTF-8 to avoid mojibake (garbled text)

--- chrome/content/scripts/smartTemplate-defaults.js
+++ chrome/content/scripts/smartTemplate-defaults.js
@@ -12,7 +12,7 @@ pref("extensions.smartTemplate4.font.size", 9); /* 8pt .templateBox font size */
 pref("extensions.smartTemplate4.parseSignature", false);
 pref("extensions.smartTemplate4.firstLastSwap", true);
 pref("extensions.smartTemplate4.header.inject", true);
-pref("extensions.smartTemplate4.defaultCharset", "ISO-8859-1");
+pref("extensions.smartTemplate4.defaultCharset", "UTF-8"); // avoid mojibake (garbled text)
 pref("extensions.smartTemplate4.signature.encoding", "UTF-8");
 pref("extensions.smartTemplate4.signature.insertDashes.plaintext", false);
 pref("extensions.smartTemplate4.signature.insertDashes.html", false);
