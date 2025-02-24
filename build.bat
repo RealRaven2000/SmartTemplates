0f4139173d9a862833a5a7d4b57556c9991a075a
RealRaven2000 <axel.grude@gmail.com>
2025-02-24 22:28:33 +0000

    version bump 4.11

--- build.bat
+++ build.bat
@@ -4,6 +4,6 @@ set /a smartTemplateRev+=1
 powershell -Command "(gc -en UTF8 manifest.json) -replace 'pre%oldRev%', 'pre%smartTemplateRev%' | Out-File manifest.json -encoding utf8"
 "C:\Program Files\7-Zip\7z" a -xr!.svn smartTemplateWeb.zip manifest.json _locales scripts html chrome locale popup st-background.* license.txt icon.png release-notes.html change-log.txt
 echo %smartTemplateRev% > revision.txt
-move smartTemplate-*.xpi "..\..\..\Test Versions\4.10\"
-powershell -Command "Start-Sleep -m 270"
-rename smartTemplateWeb.zip smartTemplate-fx-4.10.2pre%smartTemplateRev%.xpi
+move smartTemplate-*.xpi "..\..\..\Test Versions\4.11\"
+powershell -Command "Start-Sleep -m 400"
+rename smartTemplateWeb.zip smartTemplate-fx-4.11pre%smartTemplateRev%.xpi
