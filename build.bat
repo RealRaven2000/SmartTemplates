eb8e6df0c0d3349e2cb4f97d9af42673683b3370
RealRaven2000 <axel.grude@gmail.com>
2025-03-27 15:32:33 +0000

    better build

--- build.bat
+++ build.bat
@@ -5,5 +5,5 @@ powershell -Command "(gc -en UTF8 manifest.json) -replace 'pre%oldRev%', 'pre%sm
 "C:\Program Files\7-Zip\7z" a -xr!.svn smartTemplateWeb.zip manifest.json _locales scripts html chrome locale popup st-background.* license.txt icon.png release-notes.html change-log.txt
 echo %smartTemplateRev% > revision.txt
 move smartTemplate-*.xpi "..\..\..\Test Versions\4.11\"
-powershell -Command "Start-Sleep -m 400"
+powershell -Command "Start-Sleep -m 1000"
 rename smartTemplateWeb.zip smartTemplate-fx-4.11pre%smartTemplateRev%.xpi
