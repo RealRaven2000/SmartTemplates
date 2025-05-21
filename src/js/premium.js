f1ac10358325ac0745aa708d50ede5ffdd236536
RealRaven2000 <axel.grude@gmail.com>
2025-05-21 12:42:23 +0100

    4.11.4 release

--- src/js/premium.js
+++ src/js/premium.js
@@ -1,8 +1,8 @@
 
-var sales_name = "EASTER SALE"; // .saleName
-var sales_start_lbl = "April 8th"; // .saleStart
-var sales_end_lbl = "April 20th";  // .saleEnd
-var sales_end = new Date("2025-04-20");
+var sales_name = "SUMMER SALE"; // .saleName
+var sales_start_lbl = "May 2nd;" // .saleStart
+var sales_end_lbl = "June 6th";  // .saleEnd
+var sales_end = new Date("2025-06-06");
 
 /* functions that remove elements depending on the user type (from user=pro querystring ) */
 var removableItems = [
