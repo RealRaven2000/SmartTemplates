2534f1300e459e85af9f9f57d2e9e160d3b1ecc7
RealRaven2000 <axel.grude@gmail.com>
2025-08-28 13:38:18 +0100

    linter

--- chrome/content/api/Utilities/implementation.js
+++ chrome/content/api/Utilities/implementation.js
@@ -164,8 +164,6 @@ var Utilities = class extends ExtensionCommon.ExtensionAPI {
           }
           const Cc = Components.classes,
             Ci = Components.interfaces;
-          // [issue 285]
-          // util.popupLicenseNotification(mode + "_template", true, true); // save_template, load_template
                 
           let filterText; //localized text for filePicker filter menu
           
