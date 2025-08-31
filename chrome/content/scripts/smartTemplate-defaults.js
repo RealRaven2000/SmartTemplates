c9fcca2e7bb7b8ee0f220ddb4b8ef017ed31940d
RealRaven2000 <axel.grude@gmail.com>
2025-08-31 21:09:15 +0100

    typo/ copy errors

--- chrome/content/scripts/smartTemplate-defaults.js
+++ chrome/content/scripts/smartTemplate-defaults.js
@@ -65,7 +65,7 @@ pref("extensions.smartTemplate4.dateformat.day", "numeric"); // change to "2-dig
 pref("extensions.smartTemplate4.dateformat.month", "numeric"); // change to "2-digit" for forcing 2digit month in dateshort
 pref("extensions.smartTemplate4.dateformat.year", "numeric"); // change to "2-digit" for forcing 2digit dateshort only
 pref("extensions.smartTemplate4.allowScripts", false); // prohibit running Javascript unless users know what they are doing and set this to true
-/* this is a convenience function - if I change from after or during composing, delete sig */
+/* this is a convenience function - if I change from after or during composing, delete signature */
 pref("extensions.smartTemplate4.removeSigOnIdChangeAfterEdits", false);
 /* header replacement rules */
 pref("extensions.smartTemplate4.headers.unescape.quotes", true);
