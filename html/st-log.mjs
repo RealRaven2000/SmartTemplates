252a58ce626cff82fd60292525d8c49aef2a437a
RealRaven2000 <axel.grude@gmail.com>
2026-01-25 17:00:27 +0000

    clarification

--- html/st-log.mjs
+++ html/st-log.mjs
@@ -1,6 +1,6 @@
 export function logMissingFunction(txt) {
   // Log missing items for Conversion to Thunderbird 115
-  console.log(`SmartTemplates %c[issue 259] to do: %c ${txt} `, 
+  console.log(`SmartTemplates %cmissing function - to do: %c ${txt} `, 
     "color:darkred", 
     "background: darkred; color:yellow;");
 }
