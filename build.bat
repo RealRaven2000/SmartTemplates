47edf8846b33807743b53a0ea180145f4e4465dc
RealRaven2000 <axel.grude@gmail.com>
2026-08-27 23:46:43 +0100

    4.19.1 version bump

--- build.bat
+++ build.bat
@@ -6,4 +6,4 @@ powershell -Command "(gc -en UTF8 manifest.json) -replace 'pre%oldRev%', 'pre%sm
 echo %smartTemplateRev% > revision.txt
 move smartTemplate-*.xpi "..\..\..\Test Versions\4.19\"
 powershell -Command "Start-Sleep -m 1000"
-rename smartTemplateWeb.zip smartTemplate-fx-4.19pre%smartTemplateRev%.xpi
+rename smartTemplateWeb.zip smartTemplate-fx-4.19.1pre%smartTemplateRev%.xpi
